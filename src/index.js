83e4cb475fb53cad0613b8e4e865b1f9e12d11d9
AnujGautaam <101124205+AnujGautaam@users.noreply.github.com>
2023-09-30 16:47:41 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './Components/Login';
+// import Login from './Components/Login';
 import Routing from './Routing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
